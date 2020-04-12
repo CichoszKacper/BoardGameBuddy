86105cc61f0a2cf119c7e3893d96e783f72ad76a
alana-jg <alana_graham@hotmail.co.uk>
2020-04-12 11:53:09 +0100

    Draft UI for new game

--- app/src/main/java/com/boardgamebuddy/ScoreTrackerActivity.java
+++ app/src/main/java/com/boardgamebuddy/ScoreTrackerActivity.java
@@ -20,44 +20,44 @@ public class ScoreTrackerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_tracker);
 
-        Button enterBtn = (Button) findViewById(R.id.enterButton);
-        final EditText playerCount = (EditText) findViewById(R.id.playerCountEditText);
-        final LinearLayout scores = (LinearLayout) findViewById(R.id.scoresLinearLayout);
-
-
-        enterBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int players;
-                scores.removeAllViews();
-                if (playerCount.getText().toString().isEmpty()){
-                    players = 0;
-                } else{
-                    players = Integer.parseInt(playerCount.getText().toString());
-                }
-
-                for (int i=0; i< players; i++){
-                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                            LinearLayout.LayoutParams.WRAP_CONTENT);
-
-                    TextView playerName = new TextView(ScoreTrackerActivity.this);
-                    playerName.setText("Player "+ String.valueOf(i+1));
-                    playerName.setLayoutParams(params);
-                    playerName.setId(ViewCompat.generateViewId());
-                    playerName.setPadding(10,10,10,10);
-                    scores.addView(playerName);
-
-                    EditText playerScore = new EditText(ScoreTrackerActivity.this);
-                    playerScore.setLayoutParams(params);
-                    playerScore.setInputType(InputType.TYPE_CLASS_NUMBER);
-                    playerScore.setId(ViewCompat.generateViewId());
-                    playerScore.setPadding(10,10,10,10);
-                    scores.addView(playerScore);
-
-
-                }
-            }//end of onclick
-        });
+//        Button enterBtn = (Button) findViewById(R.id.enterButton);
+//        final EditText playerCount = (EditText) findViewById(R.id.playerCountEditText);
+//        final LinearLayout scores = (LinearLayout) findViewById(R.id.scoresLinearLayout);
+//
+//
+//        enterBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                int players;
+//                scores.removeAllViews();
+//                if (playerCount.getText().toString().isEmpty()){
+//                    players = 0;
+//                } else{
+//                    players = Integer.parseInt(playerCount.getText().toString());
+//                }
+//
+//                for (int i=0; i< players; i++){
+//                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+//                            LinearLayout.LayoutParams.WRAP_CONTENT);
+//
+//                    TextView playerName = new TextView(ScoreTrackerActivity.this);
+//                    playerName.setText("Player "+ String.valueOf(i+1));
+//                    playerName.setLayoutParams(params);
+//                    playerName.setId(ViewCompat.generateViewId());
+//                    playerName.setPadding(10,10,10,10);
+//                    scores.addView(playerName);
+//
+//                    EditText playerScore = new EditText(ScoreTrackerActivity.this);
+//                    playerScore.setLayoutParams(params);
+//                    playerScore.setInputType(InputType.TYPE_CLASS_NUMBER);
+//                    playerScore.setId(ViewCompat.generateViewId());
+//                    playerScore.setPadding(10,10,10,10);
+//                    scores.addView(playerScore);
+//
+//
+//                }
+//            }//end of onclick
+//        });
 
 
 
